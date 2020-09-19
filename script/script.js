c849f6734af7d1025d6cc4c0bfe77215b2f4baab
druber07 <druber07program@gmail.com>
2020-09-19 20:31:10 +0300

    product setup - Done
    Starting playing with the nav

--- script/script.js
+++ script/script.js
@@ -1,16 +1,16 @@
-const navSlide = () => {
-    const burger = document.getElementById("burger");
-    const nav = document.getElementById("navbar");
-    
-    
-
-    burger.addEventListener('click', () =>{
-        nav.classList.toggle('navbar-active{');
-        alert("asd");
-    });
+function product_show(id){
+  const container = document.getElementById("container");
+  const productName = document.getElementById("product-info-name");
+  document.getElementById("product-info-name").innerHTML = "Product no."+id;
+  console.log(productName);
+  container.classList.add("active");
+}
+function closePopup() {
+  container.classList.remove("active");
 }
 
-const app = () =>{
-    navSlide();
+function navSlide(){
+  const nav = document.getElementById("nav");
+
+  nav.classList.toggle("active-nav");
 }
-app()
