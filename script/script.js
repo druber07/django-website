30640c2c787842335f1424ddcd48626568959ef6
druber07 <druber07program@gmail.com>
2020-09-19 23:02:56 +0300

    Popup update + responsive nav
    Update for product and about us information
    Responsive nav

--- script/script.js
+++ script/script.js
@@ -1,8 +1,12 @@
 function product_show(id){
   const container = document.getElementById("container");
-  const productName = document.getElementById("product-info-name");
-  document.getElementById("product-info-name").innerHTML = "Product no."+id;
-  console.log(productName);
+  document.getElementById("popup_name").innerHTML = "Product no." + id;
+  container.classList.add("active");
+}
+function aboutUs_info() {
+  document.getElementById("popup_name").innerHTML = "About us";
+  document.getElementById("popup_text").innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada bibendum arcu vitae elementum curabitur vitae nunc sed. Ipsum consequat nisl vel pretium lectus quam id. Pellentesque elit eget gravida cum sociis natoque penatibus et. A lacus vestibulum sed arcu non. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Aliquam malesuada bibendum arcu vitae.";
   container.classList.add("active");
 }
 function closePopup() {
